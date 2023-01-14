44db0b213fcbb508b69ebcb24a3984fe3e069b52
잉퓨 <dayeon620@kakao.com>
2023-01-15 03:08:46 +0900

    [chapter5] #1 5-2 clock

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock()//웹사이트 실행 후 1초 뒤 실행되는 것을 막기 위함
